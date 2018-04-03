39a652a601c0fbb3badea5d29ab9614005ccc9a1
Jailani Abdul Rahman <jailani.har@googlemail.com>
2018-04-03 12:04:45 +0800

    Week 12 Java Web Programming (Slot 1)

---
+++ week12/mywebapplication/src/main/java/com/nep/mywebapplication/MywebapplicationApplication.java
@@ -0,0 +1,12 @@
+package com.nep.mywebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebapplicationApplication.class, args);
+	}
+}
---
+++ week12/mywebapplication/src/test/java/com/nep/mywebapplication/MywebapplicationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nep.mywebapplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MywebapplicationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
